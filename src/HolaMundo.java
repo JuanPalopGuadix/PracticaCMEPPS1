848d9d119e8695e942a75278dd95f5305ac2c976
usuario <usuario@pc20-022>
2023-09-26 11:02:12 +0200

    Bien tipado

--- src/HolaMundo.java
+++ src/HolaMundo.java
@@ -4,14 +4,9 @@ public class HolaMundo {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hola CMPPS!");
-<<<<<<< HEAD
 		System.out.println("Bienvenido al curso 23/24");
 		System.out.println("nueva linea para provocar el fallo");
-		
-=======
 		System.out.println("Hola CMPPS222!");
->>>>>>> branch 'master' of https://github.com/JuanPalopGuadix/PracticaCMEPPS1.git
-
 	}
 
 }
