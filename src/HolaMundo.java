30efc3104772018b98dfb13af4f5badceb32e626
usuario <usuario@pc20-024>
2023-09-26 10:55:53 +0200

    nueva linea

--- src/HolaMundo.java
+++ src/HolaMundo.java
@@ -5,6 +5,7 @@ public class HolaMundo {
 		// TODO Auto-generated method stub
 		System.out.println("Hola CMPPS!");
 		System.out.println("Bienvenido al curso 23/24");
+		System.out.println("nueva linea para provocar el fallo");
 		
 
 	}
